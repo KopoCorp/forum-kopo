da92e9b8714452b1618d55722049cdae88bbed98
Phobetore <broucewayn1413@protonmail.com>
2025-06-19 13:41:10 +0200

    sed path bug fix

--- enable_remote_access.sh
+++ enable_remote_access.sh
@@ -12,7 +12,8 @@ PG_HBA="$CONF_DIR/pg_hba.conf"
 SUBNET=${1:-0.0.0.0/0}
 
 # Active l'ecoute sur toutes les interfaces
-sudo sed -i "s/^#*\s*listen_addresses\s*=.*$/listen_addresses = '*'/'" "$POSTGRESQL_CONF"
+sudo sed -i "s/^#*\s*listen_addresses\s*=.*$/listen_addresses = '*'/" "$POSTGRESQL_CONF"
+
 
 # Ajoute la ligne d'autorisation si absente
 if ! grep -q "^host\s\+all\s\+all\s\+$SUBNET" "$PG_HBA"; then
