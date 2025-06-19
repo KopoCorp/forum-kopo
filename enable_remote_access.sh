97ab706563e9874f119cc426765796cc050d2dac
Phobetore <pi@kopo.systems>
2025-06-19 14:02:19 +0200

    Update enable_remote_access.sh
    another sed path bug fix

--- enable_remote_access.sh
+++ enable_remote_access.sh
@@ -15,7 +15,7 @@ SUBNET=${1:-0.0.0.0/0}
 sudo sed -i "s/^#*\s*password_encryption\s*=.*$/password_encryption = 'scram-sha-256'/" "$POSTGRESQL_CONF"
 
 # Active l'ecoute sur toutes les interfaces
-sudo sed -i "s/^#*\s*listen_addresses\s*=.*$/listen_addresses = '*'/'" "$POSTGRESQL_CONF"
+sudo sed -i "s/^#*\s*listen_addresses\s*=.*$/listen_addresses = '*'/" "$POSTGRESQL_CONF"
 
 # Ajoute la ligne d'autorisation si absente
 if ! grep -q "^host\s\+all\s\+all\s\+$SUBNET" "$PG_HBA"; then
