59cb6e2cde10814aaff585a94460f1b67bcb3bbf
π-thagore <73407004+Phobetore@users.noreply.github.com>
2025-07-11 08:41:17 +0200

    feat: add random banner when article missing image

--- views/cyber_securite.php
+++ views/cyber_securite.php
@@ -72,9 +72,7 @@
                             <?php foreach ($security_articles as $article): ?>
                                 <article class="article-card">
                                     <div class="article-card-image">
-                                        <?php if (isset($article['image_url']) && !empty($article['image_url'])): ?>
-                                            <img src="<?php echo htmlspecialchars($article['image_url']); ?>" alt="<?php echo htmlspecialchars($article['title']); ?>">
-                                        <?php endif; ?>
+                                        <img src="<?php echo htmlspecialchars(get_article_banner_url($article)); ?>" alt="<?php echo htmlspecialchars($article['title']); ?>">
                                         <div class="article-card-label" style="background-color: var(--accent-red);">CYBERSÉCURITÉ</div>
                                     </div>
                                     <div class="article-card-content">
